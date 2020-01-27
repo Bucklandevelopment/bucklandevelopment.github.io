da1a083f33a8b95c6560a89df0f2662afaae1c5b
BuckDevMent <bucklandevelopment@gmail.com>
2020-01-27 16:43:29 +0100

    New files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portafolio Buckdevment Tecnologies
-
-En esta web estática de gitlab-pages con implementación dinámica gracias a Firebase y Heroku.
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,100 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-'use strict';
-const CACHE_NAME = 'static-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v1';
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-const FILES_TO_CACHE = [
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/Build/UnityLoader.js',
-  '/Build/zpowa.data.unityweb',
-  '/Build/zpowa.json',
-  '/Build/zpowa.wasm.code.unityweb',
-  '/Build/zpowa.wasm.framework.unityweb',
-  '/UnityProgress.js',
-  '/images/add.svg',
-  '/images/favicon.ico',
-  '/images/fullscreen.png',
-  '/images/install.svg',
-  '/images/progressEmpty.Dark.png',
-  '/images/progressEmpty.Light.png',
-  '/images/progressFull.Dark.png',
-  '/images/progressFull.Light.png',
-  '/images/progressLogo.Dark.png',
-  '/images/progressLogo.Light.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-144x144.png',
-  '/images/icons/icon-152x152.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-256x256.png',
-  '/images/icons/icon-32x32.png',
-  '/images/icons/icon-512x512.png',
-  '/styles/style.css',
-  '/scripts/install.js',
-];
-
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
+importScripts(
+  "/precache-manifest.bb52dd7d37d7a3cbaedfc892b3ea2588.js"
 );
 
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        //if (key !== CACHE_NAME) {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-);
-
-  self.clients.claim();
-});
-
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  //sustituimos el request por las rutas reales que usamos ahora
-  if (evt.request.url.includes('/', '/Build')){//|| evt.request.url.includes('/Build/')) {
-    console.log('[Service Worker] Fetch (data)', evt.request.url);
-    evt.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-          return fetch(evt.request)
-              .then((response) => {
-                // If the response was good, clone it and store it in the cache.
-                if (response.status === 200) {
-                  cache.put(evt.request.url, response.clone());
-                }
-                return response;
-              }).catch((err) => {
-                // Network request failed, try to get it from the cache.
-                console.log("Errores con el fetch: " + err);
-                return cache.match(evt.request);
-              });
-        }));
-    return;
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
   }
-  evt.respondWith(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(evt.request)
-            .then((response) => {
-              return response || fetch(evt.request);
-            });
-      })
-  );
+});
 
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
